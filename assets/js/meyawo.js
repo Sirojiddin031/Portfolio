cf5f5173465f0963bca9fa31203a58c459fff14e
Sirojiddin031 <uzbeksila120321@gmail.com>
2025-08-02 23:32:05 -0700

    first commit

---
+++ assets/js/meyawo.js
@@ -0,0 +1,55 @@
+/*!
+=========================================================
+* Meyawo Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function () {
+    $(".navbar .nav-link").on('click', function (event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function () {
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+
+function setLanguage(lang) {
+    const elements = document.querySelectorAll('[data-en]');
+    elements.forEach(el => {
+        el.textContent = el.getAttribute(`data-${lang}`);
+    });
+    // brauzerga tanlangan tilni saqlaymiz
+    localStorage.setItem("language", lang);
+}
+
+// Sahifa yuklanganda oldingi tanlangan tilni tiklash
+window.addEventListener('DOMContentLoaded', () => {
+    const savedLang = localStorage.getItem("language") || "en";
+    setLanguage(savedLang);
+});
+
